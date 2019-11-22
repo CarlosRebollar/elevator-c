cd6d70cdd5f0bcbb185b1a53911dbeaa4c32b196
Carlos Palacios Rebollar <carlos.palaciosrebollar@gmail.com>
2019-11-22 17:45:44 -0600

    Added architecture to project

---
+++ Debug/source/1_APP/main.d
@@ -0,0 +1,15 @@
+source/1_APP/main.o source/1_APP/main.d: ../source/1_APP/main.c \
+ ../CMSIS/MKL25Z4.h ../CMSIS/core_cm0plus.h ../CMSIS/core_cmInstr.h \
+ ../CMSIS/cmsis_gcc.h ../CMSIS/core_cmFunc.h ../CMSIS/system_MKL25Z4.h
+
+../CMSIS/MKL25Z4.h:
+
+../CMSIS/core_cm0plus.h:
+
+../CMSIS/core_cmInstr.h:
+
+../CMSIS/cmsis_gcc.h:
+
+../CMSIS/core_cmFunc.h:
+
+../CMSIS/system_MKL25Z4.h:
---
+++ Debug/source/1_APP/mtb.d
@@ -0,0 +1,1 @@
+source/1_APP/mtb.o source/1_APP/mtb.d: ../source/1_APP/mtb.c
